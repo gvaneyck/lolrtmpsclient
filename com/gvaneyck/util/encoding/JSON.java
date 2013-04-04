0d0554297444aa53f11e8903a88e16d914fc3ae3
Gabriel Van Eyck <gvaneyck@users.noreply.github.com>
2013-04-04 18:04:01 +0000

    Some improvements to JSON parsing

--- com/gvaneyck/util/encoding/JSON.java
+++ com/gvaneyck/util/encoding/JSON.java
@@ -1,243 +1,243 @@
 package com.gvaneyck.util.encoding;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-import com.gvaneyck.rtmp.TypedObject;
-
 /**
- * Simple JSON parser
- * Assumes the JSON is properly structured
- * 
+ * Simple JSON parser. Throws a RuntimeException on parsing error
+ *
  * @author Gabriel Van Eyck
  */
-public class JSON
-{
-	/**
-	 * Parses JSON from a string
-	 * 
-	 * @param json The JSON to parse
-	 * @return The parsed object
-	 */
-	public static Object parse(String json)
-	{
-		if (json == null)
-			return null;
-		
-		LinkedList<Character> buff = new LinkedList<Character>();
-		for (int i = 0; i < json.length(); i++)
-			buff.add(json.charAt(i));
-		return parse(buff);
-	}
+public class JSON {
+    /**
+     * Parses JSON from a string
+     *
+     * @param json The JSON to parse
+     * @return The parsed object
+     */
+    public static Object parse(String json) {
+        if (json == null)
+            return null;
 
-	/**
-	 * Parses JSON from a linked list
-	 * 
-	 * @param json The JSON to parse
-	 * @return The parsed object
-	 */
-	private static Object parse(LinkedList<Character> json)
-	{
-		char c = json.removeFirst();
-		while (c == ' ')
-			c = json.removeFirst();
-		
-		switch (c)
-		{
-		case '{':
-			return parseObject(json);
+        LinkedList<Character> buff = new LinkedList<Character>();
+        for (int i = 0; i < json.length(); i++)
+            buff.add(json.charAt(i));
+        return parse(buff);
+    }
 
-		case '[':
-			return parseArray(json);
+    /**
+     * Parses JSON from a linked list
+     *
+     * @param json The JSON to parse
+     * @return The parsed object
+     */
+    private static Object parse(LinkedList<Character> json) {
+        Character c = removeNonSpace(json);
+        switch (c) {
+        case '{':
+            return parseObject(json);
 
-		case '"':
-			return parseString(json);
-			
-		default:
-			json.addFirst(c);
-			return parseOther(json);
-		}
-	}
-	
-	/**
-	 * Parses a JSON object
-	 * Assumes the opening curly brace has been consumed
-	 * 
-	 * @param json The JSON to parse
-	 * @return The parsed object as a map
-	 */
-	private static TypedObject parseObject(LinkedList<Character> json)
-	{
-		TypedObject ret = new TypedObject(null);
+        case '[':
+            return parseArray(json);
 
-		char c = json.removeFirst();
-		if (c == '}') // Check for empty object
-			return ret;
-		json.addFirst(c);
-		
-		do
-		{
-			json.removeFirst(); // Read quote
-			String key = parseString(json);
-			json.removeFirst(); // Read colon
-			Object val = parse(json);
-			ret.put(key, val);
-			
-			c = json.removeFirst();
-		}
-		while (c != '}');
-		
-		return ret;
-	}
-	
-	/**
-	 * Parses a JSON array
-	 * Assumes the opening square bracket has been consumed
-	 * 
-	 * @param json The JSON to parse
-	 * @return The parsed array
-	 */
-	private static Object[] parseArray(LinkedList<Character> json)
-	{
-		char c = json.removeFirst();
-		if (c == ']') // Check for empty array
-			return new Object[0];
-		json.addFirst(c);
-		
-		ArrayList<Object> temp = new ArrayList<Object>();
-		
-		do
-		{
-			temp.add(parse(json));
-			c = json.removeFirst();
-		}
-		while (c != ']');
-		
-		Object[] ret = new Object[temp.size()];
-		for (int i = 0; i < temp.size(); i++)
-			ret[i] = temp.get(i);
-		
-		return ret;
-	}
+        case '"':
+            return parseString(json);
 
-	/**
-	 * Parses a JSON string
-	 * Assumes the opening double quote has been consumed
-	 * 
-	 * @param json The JSON to parse
-	 * @return The parsed string
-	 */
-	private static String parseString(LinkedList<Character> json)
-	{
-		boolean postBackslash = false;
-		StringBuilder buff = new StringBuilder();
-		char c;
+        default:
+            json.addFirst(c);
+            return parseOther(json);
+        }
+    }
 
-		while ((c = json.removeFirst()) != '"' || postBackslash)
-		{
-			if (!postBackslash)
-			{
-				if (c == '\\')
-					postBackslash = true;
-				else
-					buff.append(c);
-			}
-			else
-			{
-				switch (c)
-				{
-				case '"':
-					buff.append('"');
-					break;
+    /**
+     * Parses a JSON object
+     * Assumes the opening curly brace has been consumed
+     *
+     * @param json The JSON to parse
+     * @return The parsed object as a map
+     */
+    private static ObjectMap parseObject(LinkedList<Character> json) {
+        ObjectMap ret = new ObjectMap();
 
-				case '\\':
-					buff.append('\\');
-					break;
+        // Check for empty objects
+        Character c = removeNonSpace(json);
+        if (c == '}')
+            return ret;
+        json.addFirst(c);
 
-				case '/':
-					buff.append('/');
-					break;
+        do {
+            c = removeNonSpace(json);
+            if (c != '"')
+                throw new JSONParsingException("JSON object member name did not start with a double quote: '" + c + "'");
+            String key = parseString(json);
 
-				case 'b':
-					buff.append('\b');
-					break;
+            c = removeNonSpace(json);
+            if (c != ':')
+                throw new JSONParsingException("JSON object missing colon for value or member: '" + c + "'");
+            Object val = parse(json);
 
-				case 'f':
-					buff.append('\f');
-					break;
+            ret.put(key, val);
+        } while ((c = removeNonSpace(json)) == ',');
+           
+        if (c != '}')
+            throw new JSONParsingException("JSON object did not end with a curly brace: '" + c + "'");
 
-				case 'n':
-					buff.append('\n');
-					break;
+        return ret;
+    }
 
-				case 'r':
-					buff.append('\r');
-					break;
+    /**
+     * Parses a JSON array
+     * Assumes the opening square bracket has been consumed
+     *
+     * @param json The JSON to parse
+     * @return The parsed array
+     */
+    private static Object[] parseArray(LinkedList<Character> json) {
+        // Check for empty array
+        Character c = removeNonSpace(json);
+        if (c == ']')
+            return new Object[0];
+        json.addFirst(c);
 
-				case 't':
-					buff.append('\t');
-					break;
+        ArrayList<Object> temp = new ArrayList<Object>();
 
-				case 'u':
-					StringBuilder temp = new StringBuilder();
-					for (int i = 0; i < 4; i++)
-						temp.append(json.removeFirst());
-					buff.append((char)Integer.parseInt(temp.toString(), 16));
-					break;
-				}
+        do {
+            temp.add(parse(json));
+            c = removeNonSpace(json);
+        } while (c == ',');
+           
+        if (c != ']')
+            throw new JSONParsingException("JSON array did not end with a square bracket: '" + c + "'");
 
-				postBackslash = false;
-			}
-		}
+        Object[] ret = new Object[temp.size()];
+        for (int i = 0; i < temp.size(); i++)
+            ret[i] = temp.get(i);
 
-		return buff.toString();
-	}
+        return ret;
+    }
 
-	/**
-	 * Parses JSON numbers, booleans, and null
-	 * 
-	 * @param json The JSON to parse
-	 * @return The parsed object
-	 */
-	private static Object parseOther(LinkedList<Character> json)
-	{
-		char c = json.removeFirst();
-		switch (c)
-		{
-		case 't':
-			for (int i = 0; i < 3; i++)
-				json.removeFirst();
-			return true;
+    /**
+     * Parses a JSON string
+     * Assumes the opening double quote has been consumed
+     *
+     * @param json The JSON to parse
+     * @return The parsed string
+     */
+    private static String parseString(LinkedList<Character> json) {
+        boolean postBackslash = false;
+        StringBuilder buff = new StringBuilder();
 
-		case 'f':
-			for (int i = 0; i < 4; i++)
-				json.removeFirst();
-			return false;
+        Character c;
+        while ((c = json.removeFirst()) != '"' || postBackslash) {
+            if (!postBackslash) {
+                if (c == '\\')
+                    postBackslash = true;
+                else
+                    buff.append(c);
+            }
+            else {
+                switch (c) {
+                case '"':
+                    buff.append('"');
+                    break;
 
-		case 'n':
-			for (int i = 0; i < 3; i++)
-				json.removeFirst();
-			return null;
+                case '\\':
+                    buff.append('\\');
+                    break;
 
-		default:
-			StringBuilder buff = new StringBuilder();
-			boolean isInt = true;
-			while (c == '-' || c >= '0' && c <= '9' || c == '.')
-			{
-				if (c == '.')
-					isInt = false;
-				buff.append(c);
-				c = json.removeFirst();
-			}
-			json.addFirst(c);
+                case '/':
+                    buff.append('/');
+                    break;
 
-			try
-			{
-				if (isInt)
-					return Integer.parseInt(buff.toString());
-			}
-			catch (NumberFormatException e) { }
+                case 'b':
+                    buff.append('\b');
+                    break;
 
-			return Double.parseDouble(buff.toString());
-		}
-	}
+                case 'f':
+                    buff.append('\f');
+                    break;
+
+                case 'n':
+                    buff.append('\n');
+                    break;
+
+                case 'r':
+                    buff.append('\r');
+                    break;
+
+                case 't':
+                    buff.append('\t');
+                    break;
+
+                case 'u':
+                    StringBuilder temp = new StringBuilder();
+                    for (int i = 0; i < 4; i++)
+                        temp.append(json.removeFirst());
+                    buff.append((char)Integer.parseInt(temp.toString(), 16));
+                    break;
+                }
+
+                postBackslash = false;
+            }
+        }
+
+        return buff.toString();
+    }
+
+    /**
+     * Parses JSON numbers, booleans, and null
+     *
+     * @param json The JSON to parse
+     * @return The parsed object
+     */
+    private static Object parseOther(LinkedList<Character> json) {
+        Character c = removeNonSpace(json);
+        switch (c) {
+        case 't':
+            for (int i = 0; i < 3; i++)
+                json.removeFirst();
+            return true;
+
+        case 'f':
+            for (int i = 0; i < 4; i++)
+                json.removeFirst();
+            return false;
+
+        case 'n':
+            for (int i = 0; i < 3; i++)
+                json.removeFirst();
+            return null;
+
+        default:
+            StringBuilder buff = new StringBuilder();
+            boolean isInt = true;
+            while (c == '-' || c >= '0' && c <= '9' || c == '.') {
+                if (c == '.')
+                    isInt = false;
+                buff.append(c);
+                c = json.removeFirst();
+            }
+            json.addFirst(c);
+               
+            if (buff.length() == 0)
+                throw new JSONParsingException("Tried parsing number but got: '" + c + "'");
+
+            if (isInt)
+                return Integer.parseInt(buff.toString());
+            else
+                return Double.parseDouble(buff.toString());
+        }
+    }
+
+    /**
+     * Extracts the first non-whitespace character from the JSON string
+     *
+     * @param json The JSON string
+     * @return The first non-whitespace character
+     */
+    private static Character removeNonSpace(LinkedList<Character> json) {
+        Character ret;
+        while (Character.isWhitespace(ret = json.removeFirst())) {}
+        return ret;
+    }
 }
