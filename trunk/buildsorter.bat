ab1f07eb2db9b0103bed28f8e301cde8a67ec36a
vaneyckster@gmail.com <vaneyckster@gmail.com@cd7f3fca-efc3-1158-8b9b-82da71c0faa7>
2013-05-27 22:38:04 +0000

    Update batch files to work with new directory structure

--- trunk/buildsorter.bat
+++ trunk/buildsorter.bat
@@ -1,5 +1,7 @@
 mkdir build
-javac -d build com\gvaneyck\runesorter\RunePageSorter.java
-cd build
+cd src
+javac -d ..\build com\gvaneyck\runesorter\RunePageSorter.java
+cd ..\build
 jar -cvfm ..\LoLPageSorter.jar ..\LoLPageSorter.MF com
 rmdir /S /Q com
+cd ..
