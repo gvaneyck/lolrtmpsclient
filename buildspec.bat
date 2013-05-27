46cc3ff9e7be6cf604be024eef32c9bc6e193c6d
Gabriel Van Eyck <gvaneyck@users.noreply.github.com>
2013-05-27 22:38:04 +0000

    Update batch files to work with new directory structure

--- buildspec.bat
+++ buildspec.bat
@@ -1,5 +1,7 @@
 mkdir build
-javac -d build com\gvaneyck\spectate\SpectateAnyone.java
-cd build
+cd src
+javac -d ..\build com\gvaneyck\spectate\SpectateAnyone.java
+cd ..\build
 jar -cvfm ..\SpectateAnyone.jar ..\SpectateAnyone.MF com
 rmdir /S /Q com
+cd ..
