6a9fdabdb09356c807125439974404dbac3a6bfc
vaneyckster@gmail.com <vaneyckster@gmail.com@cd7f3fca-efc3-1158-8b9b-82da71c0faa7>
2014-12-03 19:13:20 +0000

    Adding missing file

---
+++ trunk/src/com/gvaneyck/runesorter/Talent.java
@@ -0,0 +1,10 @@
+package com.gvaneyck.runesorter;
+
+public class Talent {
+    public int id;
+    public String desc;
+    public Integer preReq;
+    public int row;
+    public int rank;
+    public int max;
+}
